945df3282d7c36e1531cce9fe96cdccbee15087c
Bob-development <proinvoke228@gmail.com>
2023-12-10 00:28:24 +0200

    asd

---
+++ core/append.ts
@@ -0,0 +1,2 @@
+export const append = (element: HTMLElement, children) => element.append(children);
+export const appendMany = (element: HTMLElement, children) => element.append(children);
---
+++ src/main.ts
@@ -0,0 +1,10 @@
+import { Component, AdvancedComponent } from "../core/Component";
+
+import './style.css'
+
+const a = new Component({
+  tagName: 'div',
+  classname: 'huy',
+})
+
+console.log(a);
